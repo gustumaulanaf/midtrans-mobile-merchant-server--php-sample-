4327586c354121ed91368d17d75864f1717049cf
Gustu Maulana F <43457312+gustumaulanaf@users.noreply.github.com>
2021-07-05 15:15:03 +0700

    Add server key

--- charge/index.php
+++ charge/index.php
@@ -1,6 +1,6 @@
 <?php
 // Set your server key (Note: Server key for sandbox and production mode are different)
-$server_key = '<server key>';
+$server_key = 'SB-Mid-server-KxwaS6kCr2VZGTY9fjJrwbYc';
 // Set true for production, set false for sandbox
 $is_production = false;
 
